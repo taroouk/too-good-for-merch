515c31e1b4c0dbc6ff138dfd9d3e8aa4f83108fb
taroouk <tarekelfanty@gmail.com>
2026-06-21 17:28:38 +0300

    up

--- app/orders/[orderId]/page.tsx
+++ app/orders/[orderId]/page.tsx
@@ -1,23 +1,3 @@
-import { prisma } from "src/lib/prisma";
-import { notFound } from "next/navigation";
-
-export default async function OrderPage({ params }: any) {
-  const order = await prisma.order.findUnique({
-    where: { id: params.orderId },
-    include: { items: true },
-  });
-
-  if (!order) notFound();
-
-  return (
-    <main className="p-10">
-      <h1 className="text-xl font-bold">Order Detail</h1>
-
-      <p className="mt-2">Order: {order.orderNumber}</p>
-
-      <div className="mt-4 border p-4 rounded-xl">
-        {order.items[0]?.product}
-      </div>
-    </main>
-  );
+export default function OrdersPage() {
+  return <div>Orders Page</div>;
 }
